87db4ab16fa1ec8739573adf07424d1d4ce0ed79
David Makin <sleep-er@sleep-er.co.uk>
2013-07-29 09:25:38 +0100

    basic first commit

---
+++ chocolatey-setup.ps1
@@ -0,0 +1,38 @@
+# Install Chocolatey
+
+"Installing Chocolatey..."
+iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
+SET PATH=%PATH%;%systemdrive%\chocolatey\bin
+"Installing Chocolatey - Done..."
+
+cinst 7zip
+cinst notepadplusplus
+cinst Firefox
+cinst filezilla
+cinst FoxitReader
+cinst gimp
+cinst truecrypt
+cinst putty
+cinst virtualbox
+cinst vlc
+cinst dropbox
+cinst SkyDrive 
+cinst launchy
+cinst Calibre
+cinst pidgin 
+cinst defraggler 
+cinst imgburn 
+
+cinst steam 
+cinst minecraft 
+cinst gfwlive 
+cinst Teamspeak3 
+cinst wassapp 
+cinst viber 
+
+cinst fiddler
+cinst sublimetext2
+cinst vagrant
+cinst SourceCodePro 
+
+
